9efbedde6ff8bd90e00e4a890ef8165746d3d222
unknown <m9500013757@gmail.com>
2022-09-10 00:04:59 +0300

    add task

---
+++ Task/Program.cs
@@ -0,0 +1,34 @@
+﻿
+string[] input = { "hello", "2", "world", ":-)" };
+string[] output = new string[input.Length];
+
+Main(input, output);
+
+//решение задачи
+void Main(string[] input, string[] output)
+{
+    int a = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] != null && input[i].Length <= 3)
+        {
+            output[a] = input[i];
+            a++;
+        }
+    }
+    PrintOneDimArray(output);
+}
+
+//метод для печати одномерного массива
+void PrintOneDimArray(string[] output)
+{
+    int i = 0;
+    while (i < output.Length)
+    {
+        if (output[i] != null)
+        {
+            Console.Write(output[i] + " ");
+        }
+        i++;
+    }
+}
